6a8a79adf1fdc532c753073e80f1802ea5e61349
Gopal Agarwal <gopalmittal2003@gmail.com>
2025-10-06 12:53:52 +0530

    Multi Processing

--- Multi_Threading_Processing/Multi_Processing_with_ProcessPoolExecutor.py
+++ Multi_Threading_Processing/Multi_Processing_with_ProcessPoolExecutor.py
@@ -1,16 +1,21 @@
 from concurrent.futures import ProcessPoolExecutor
 import time
 
+
 def square_number(number):
     time.sleep(1)
     return f"Square: {number * number}"
 
 numbers = [1,2,3,4,5]
 
-with ProcessPoolExecutor(max_workers=3) as executor:
-    results = executor.map(square_number,numbers)
+t = time.time()
 
+if __name__ == "__main__":
+    with ProcessPoolExecutor(max_workers=5) as executor:
+        results = executor.map(square_number,numbers)
 
+    for result in results:
+        print(result)
 
-for result in results:
-    print(result)
+    Total_time = time.time() - t
+    print(Total_time)
