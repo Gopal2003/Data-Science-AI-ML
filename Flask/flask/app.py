4eede27d65a7cf70e32a671d212d85795f28f6df
Gopal Agarwal <gopalmittal2003@gmail.com>
2025-10-18 18:28:28 +0530

    Getting started with Flask Framework

--- Flask/flask/app.py
+++ Flask/flask/app.py
@@ -3,13 +3,21 @@ from flask import Flask
 '''
  Creating the Instance of Flask Class,
  Which will be your WSGI (Web Server GateWay Interface) application.
+ Web Server : Werkzeug server(Flask's built-in Web Server)
 '''
 
-app = Flask(__name__ )
+app = Flask(__name__ ) # Web Application (WSGI)
 
 @app.route("/")
 def Welcome():
-    return "Welcome To the Flask Course"
+    return "Welcome To the Flask Course... "
 
+@app.route("/Index")
+def WelcomeIndex():
+    return "Welcome To the Index Page... "
+
+'''
+debug=Ture automatically make the changes appear without the need of restart.
+'''
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
