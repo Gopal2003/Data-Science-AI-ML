6124ea97df39d6273331ee768d6718eb1027d6d8
Gopal Agarwal <gopalmittal2003@gmail.com>
2025-10-19 10:30:55 +0530

    Jinja2 Templating Engine Implementation

---
+++ Flask/flask/Main.py
@@ -0,0 +1,25 @@
+from flask import Flask,render_template
+
+'''
+ Creating the Instance of Flask Class,
+ Which will be your WSGI (Web Server GateWay Interface) application.
+ Web Server : Werkzeug server(Flask's built-in Web Server)
+
+ render_template : This template is responsible to redirect to another page like HTMl 
+'''
+
+app = Flask(__name__ ) # Web Application (WSGI)
+
+@app.route("/")
+def Welcome():
+    return "<html><H3>Welcome to the Flask Course </H3></html>"
+
+@app.route("/Index")
+def WelcomeIndex():
+    return render_template('index.html')
+
+'''
+debug=Ture automatically make the changes appear without the need of restart.
+'''
+if __name__ == "__main__":
+    app.run(debug=True)
